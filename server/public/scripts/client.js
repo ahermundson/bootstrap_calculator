3003acdbcf2415803353e1bca197f20450c3a6d0
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2016-11-12 12:26:47 -0600

    seperated routes for each operator

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,11 +2,9 @@ $(document).ready(function(){
   var displayString = "";
   var lowerDisplayString = "";
   var mathObject = {};
-  var operatorArray = [];
   var numberCounter = 1;
 
-
-
+  //update number in display
   $('.number').on('click', function() {
     lowerDisplayString += $(this).attr('id');
     displayString += $(this).attr('id');
@@ -14,12 +12,15 @@ $(document).ready(function(){
     $('#lower-display').text(lowerDisplayString);
   });
 
+
+  //clear out text in display
   $('#clear').on('click', function() {
     displayString = "";
     $('#display').text("0");
     $('#lower-display').text("0");
   });
 
+  //add operator to object. if this is not the first operator selected, run proper calculation, put the result in mathObject.firstNumber, THEN put this operator in mathObject.operartor
   $('.operator').on('click', function() {
     lowerDisplayString = lowerDisplayString + " " + $(this).text() + " ";
     $('#lower-display').text(lowerDisplayString);
@@ -29,17 +30,23 @@ $(document).ready(function(){
         mathObject.operator = $(this).attr('id');
         displayString = "";
         console.log(mathObject);
+        numberCounter++;
+      }
+      else {
+        //ajax request to get result of previous calculation
+
       }
     }
   });
 
   $('.operator-equals').on('click', function() {
+    //confirm that an operator has been selected
     if (mathObject.hasOwnProperty('operator')) {
       mathObject.secondNumber = Number(displayString);
       console.log("operator-equals listener", mathObject);
       $.ajax({
         type: 'POST',
-        url: '/math',
+        url: '/math/' + mathObject.operator,
         data: mathObject,
         success: function(mathObject) {
           getCompleted();
@@ -71,54 +78,3 @@ function clearForm() {
   $('form').find('input[type=number]').val("");
   $('#result').text("");
 }
-
-
-
-
-
-
-// $(document).ready(function() {
-//
-//   $('.operator').on('click', function(){
-//     event.preventDefault();
-//     var numbersObject = {};
-//     var operation = $(this).attr('id');
-//     $.each($('#calculatorInput').serializeArray(), function(i, field) {
-//       numbersObject[field.name] = field.value;
-//     });
-//     numbersObject.type = operation;
-//     $.ajax({
-//       type: 'POST',
-//       url: '/math',
-//       data: numbersObject,
-//       success: function(numbersObject) {
-//         getCompleted();
-//       }
-//     });
-//   });
-//
-//   $('#clear').on('click', clearForm);
-//
-// });
-//
-//
-// function getCompleted() {
-//   $.ajax({
-//     type: 'GET',
-//     url: '/math',
-//     success: function(data) {
-//       console.log('got the completed data!');
-//       appendCompleted(data);
-//     }
-//   });
-// }
-//
-// function appendCompleted(data) {
-//   console.log(data);
-//   $('#result').text(data.number);
-// }
-//
-// function clearForm() {
-//   $('form').find('input[type=number]').val("");
-//   $('#result').text("");
-// }
--- server/routes/math.js
+++ server/routes/math.js
@@ -1,36 +1,62 @@
 var express = require('express');
 var router = express.Router();
-
+var calculateObject = {};
 var result = {number: 0};
 
-router.post('/', function (req, res) {
-  console.log(req.body);
-  console.log(req.body.operator);
-  var calculateObject = req.body;
-  console.log("object ", calculateObject);
-  switch(calculateObject.operator) {
-    case "add":
-      result.number = Number(calculateObject.firstNumber) + Number(calculateObject.secondNumber);
-      console.log(result);
-      break;
 
-    case "subtract":
-      result.number = Number(calculateObject.firstNumber) - Number(calculateObject.secondNumber);
-      console.log(result);
-      break;
-
-    case "divide":
-      result.number = Number(calculateObject.firstNumber) / Number(calculateObject.secondNumber);
-      console.log(result);
-      break;
-
-    case "multiply":
-      result.number = Number(calculateObject.firstNumber) * Number(calculateObject.secondNumber);
-      console.log(result);
-      break;
-  }
+router.post('/add', function (req, res) {
+  calculateObject = req.body;
+  result.number = Number(calculateObject.firstNumber) + Number(calculateObject.secondNumber);
+  console.log("add route");
   res.sendStatus(201);
 });
+
+router.post('/subtract', function (req, res){
+  calculateObject = req.body;
+  result.number = Number(calculateObject.firstNumber) - Number(calculateObject.secondNumber);
+  console.log("subtract route");
+  res.sendStatus(201);
+});
+
+router.post('/multiply', function (req, res) {
+  calculateObject = req.body;
+  result.number = Number(calculateObject.firstNumber) * Number(calculateObject.secondNumber);
+  console.log("multiply route");
+  res.sendStatus(201);
+});
+
+ router.post('/divide', function (req, res) {
+  calculateObject = req.body;
+  result.number = Number(calculateObject.firstNumber) / Number(calculateObject.secondNumber);
+  console.log("divide route");
+  res.sendStatus(201);
+ });
+
+// router.post('/', function (req, res) {
+//
+//   switch(calculateObject.operator) {
+//     case "add":
+//       result.number = Number(calculateObject.firstNumber) + Number(calculateObject.secondNumber);
+//       console.log(result);
+//       break;
+//
+//     case "subtract":
+//       result.number = Number(calculateObject.firstNumber) - Number(calculateObject.secondNumber);
+//       console.log(result);
+//       break;
+//
+//     case "divide":
+//       result.number = Number(calculateObject.firstNumber) / Number(calculateObject.secondNumber);
+//       console.log(result);
+//       break;
+//
+//     case "multiply":
+//       result.number = Number(calculateObject.firstNumber) * Number(calculateObject.secondNumber);
+//       console.log(result);
+//       break;
+//   }
+//   res.sendStatus(201);
+// });
 
 
 router.get('/', function (req, res) {
